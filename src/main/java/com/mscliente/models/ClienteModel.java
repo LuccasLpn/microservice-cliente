d6702c93b77eccfbafe8ef7e379d549ccf40d843
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-12 00:38:49 -0300

    Created Models

---
+++ src/main/java/com/mscliente/models/ClienteModel.java
@@ -0,0 +1,25 @@
+package com.mscliente.models;
+
+import lombok.Data;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "TB_CLIENTE")
+public class ClienteModel implements Serializable {
+
+    @Serial
+    private  static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @Email
+    private String email;
+
+}
